dd8a7cee0cc26685e256f268905ec6a2f4695f97
alan4ik <al4ers@gmail.com>
2023-04-11 00:17:44 +0300

    123

--- unitest_argv_1.py
+++ unitest_argv_1.py
@@ -1,52 +1,26 @@
-# Конечно, вот пример кода unittest на языке Python, 
-# который позволяет передавать аргументы командной строки:
-
-# В этом примере мы получаем аргументы командной строки с помощью sys.argv, 
-# затем преобразуем их в словарь и задаем значения по умолчанию. 
-# Затем мы удаляем аргументы из словаря kwargs, чтобы они не были переданы unittest.
-
-# Затем мы устанавливаем значения аргументов в тестовом классе MyTest, 
-# используя статические переменные. Наконец, мы запускаем unittest с 
-# помощью unittest.main() и передаем ему все аргументы командной строки, 
-# кроме arg1 и arg2, используя параметр argv.
-
 import unittest
-import sys
+import argparse
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--foo", default="default_foo")
+    return parser
 
 class MyTest(unittest.TestCase):
+    def __init__(self, methodName, namespace):
+        super().__init__(methodName)
+        self.namespace = namespace
 
-    def test_one(self):
-        print("arg1:", self.arg1)
-        self.assertEqual(self.arg1, "value1")
-
-    def test_two(self):
-        print("arg2:", self.arg2)
-        self.assertEqual(self.arg2, "value2")
+    def test_something(self):
+        print(f'Testing something with foo={self.namespace.foo}')
+        self.assertEqual(1, 1)
 
 if __name__ == '__main__':
-    # Получаем аргументы командной строки
-    args = sys.argv[1:]
+    parser = createParser()
+    namespace = parser.parse_args()
 
-    # Создаем пустой словарь для аргументов
-    kwargs = {}
+    suite = unittest.TestSuite()
+    suite.addTest(MyTest('test_something', namespace=namespace))
 
-    # Ищем аргументы в формате --key=value
-    for arg in args:
-        if '=' in arg:
-            key, value = arg.split('=')
-            kwargs[key] = value
-
-    # Задаем значения аргументов по умолчанию
-    arg1 = kwargs.get('arg1', 'value1')
-    arg2 = kwargs.get('arg2', 'value2')
-
-    # Удаляем аргументы из kwargs, чтобы они не были переданы unittest
-    del kwargs['arg1']
-    del kwargs['arg2']
-
-    # Передаем аргументы в класс MyTest
-    MyTest.arg1 = arg1
-    MyTest.arg2 = arg2
-
-    # Запускаем unittest
-    unittest.main(argv=[sys.argv[0]] + args, **kwargs)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
